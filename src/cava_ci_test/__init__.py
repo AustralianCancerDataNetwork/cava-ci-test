85318341f8e4e32315ee60c97c268550fdbcfe66
nicoloesch <70891565+nicoloesch@users.noreply.github.com>
2026-07-15 12:54:02 +1000

    Add division test-case (#19)

--- src/cava_ci_test/__init__.py
+++ src/cava_ci_test/__init__.py
@@ -1,8 +1,9 @@
 """cava-ci-test: minimal package for validating cava-devops CI/CD pipelines."""
-from .feat import add, subtract, multiply
+from .feat import add, subtract, multiply, divide
 
 __all__ = [
     "add",
     "subtract",
-    "multiply"
+    "multiply",
+    "divide",
 ]
