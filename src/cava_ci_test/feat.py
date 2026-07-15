6267c17f0c30f6b1db4e968f5469f121544f0243
Nico Loesch <n.loesch@unsw.edu.au>
2026-07-15 01:00:28 +0000

    Add subtraction ability

--- src/cava_ci_test/feat.py
+++ src/cava_ci_test/feat.py
@@ -1,3 +1,7 @@
 def add(x: int, y: int) -> int:
     """Add two integers. Dummy feature for testing CI/CD pipelines."""
     return x + y
+
+def subtract(x: int, y: int) -> int:
+    """Subtract two integers. Dummy feature for testing CI/CD pipelines."""
+    return x - y
