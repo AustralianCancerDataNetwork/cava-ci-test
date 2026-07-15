96c83a6aacb0259ad81a9cac6dc3deb982c4da42
nicoloesch <70891565+nicoloesch@users.noreply.github.com>
2026-07-15 12:19:17 +1000

    Add division (#13)

--- src/cava_ci_test/feat.py
+++ src/cava_ci_test/feat.py
@@ -9,3 +9,7 @@ def subtract(x: int, y: int) -> int:
 def multiply(x: int, y: int) -> int:
     """Multiply two integers. Dummy feature for testing CI/CD pipelines."""
     return x * y
+
+def divide(x: int, y: int) -> float:
+    # add error on purpose so we can see the fix bit
+    return x / y
